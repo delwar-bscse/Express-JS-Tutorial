2c2b2b0e9f474271c9c7ff0e513d903343c8f4e1
M D Hossain <delwarbscse@gmail.com>
2022-11-01 23:56:59 +0600

    GET POST PUT DELETE

---
+++ Lesson-1/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+app.get("/",(req,res)=>{
+    res.send("I am get request at home route");
+    res.end();
+});
+app.post("/", (req,res)=>{
+    res.send("I am post request at home route");
+});
+app.put("/",(req,res)=>{
+    res.send("I am put request at home route");
+});
+app.delete("/",(req,res)=>{
+    res.send("I am delete request at home route");
+});
+
+module.exports = app;
