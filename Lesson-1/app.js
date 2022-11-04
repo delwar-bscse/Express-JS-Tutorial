44cf021475a04cfd27b14124fc922d89cdbc18a2
M D Hossain <delwarbscse@gmail.com>
2022-11-05 00:42:10 +0600

    send JSON file

--- Lesson-1/app.js
+++ Lesson-1/app.js
@@ -4,8 +4,17 @@ const userRouter = require('./routes/users.route');
 
 app.use("/api/user",userRouter);
 app.get("/",(req,res)=>{
-    res.send("I am get request at Home route");
+    res.send("I am  Home route");
 });
+app.get("/register",(req,res)=>{
+    res.status(200).json({
+        massage:"I am register route",
+        statusCode: 200
+    });
+});
+app.get("/login",(req,res)=>{
+    res.send("I am login route")
+})
 app.get("*",(req,res)=>{
     res.send("404!!! Not found URL");
 });
