9190b28587f1af9353b87cb01b95077140bdeaa6
M D Hossain <delwarbscse@gmail.com>
2022-11-28 07:21:57 +0600

    Can't run the code

--- Lesson-1/app.js
+++ Lesson-1/app.js
@@ -1,23 +1,12 @@
 const express = require('express');
 const app = express();
-const userRoute = require('./routes/users.route');
+const userRouter = require('./routes/users.route');
 
-app.use("/api",userRoute);
+app.use("/api",userRouter);
 
-app.get("/",(req,res)=>{
+app.get('/',(req,res)=>{
     res.statusCode = 202;
     res.sendFile(__dirname+"/views/index.html");
-})
-app.get("/login",(req,res)=>{
-    res.statusCode = 202;
-    res.sendFile(__dirname+"/views/login.html");
 });
-app.get("/register",(req,res)=>{
-    res.statusCode = 202;
-    res.sendFile(__dirname+"/views/register.html");
-});
-app.get("*",(req,res)=>{
-    res.end("<h1>404!!! Error Page</h1>");
-})
 
-module.exports = app;
+module.export = app;
--- Lesson-1/routes/users.route.js
+++ Lesson-1/routes/users.route.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-router.get("/user",(req,res)=>{
+router.get('/user', (req,res)=>{
     res.status(200).json({
         Name : "M D Hossain",
-        Age : "25"
-    })
+        Age : 25
+    });
 });
 
 module.exports = router;
