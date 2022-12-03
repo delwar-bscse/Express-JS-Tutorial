07927d964b094d83a79dd181f9ec81549b4c91ca
M D Hossain <delwarbscse@gmail.com>
2022-12-03 23:13:49 +0600

    form post

--- Lesson-2/index.js
+++ Lesson-2/index.js
@@ -7,12 +7,16 @@ const PORT = 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.post("/user",(req,res)=>{
-    const name = req.body.name;
+app.get("/register",(req,res)=>{
+    res.sendFile(__dirname+"/index.html");
+});
+
+app.post("/register",(req,res)=>{
+    const name = req.body.fullName;
     const age = req.body.age;
-    res.send(`Welcome ${name}. You're ${age} years old.`);
-})
+    res.send(`<h1>Name : ${name} </br> Age : ${age}</h1>`);
+});
 
 app.listen(PORT, ()=>{
     console.log(`Server is running at http://localhost:${PORT}`);
-}) 
+});
